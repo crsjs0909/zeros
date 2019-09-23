317d1f5f132b103ef9ec018d234d95117172de49
oleg kin <noklige@gmail.com>
2019-09-23 16:40:58 +0300

    feat: function implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+
+    let tokens = expression.split("*");
+    let five = 0;
+    let odd = 0;
+    for (let i=0; i<tokens.length; i++) {
+        let nums=facToArray(tokens[i]);
+        for (let num=0; num<nums.length; num++){
+            five+=extractPow(nums[num], 5);
+            odd+=extractPow(nums[num], 2);
+        }
+    }
+    return Math.min(five, odd);
+}
+
+function extractPow(num, mul) {
+    let ecx = 0;
+    while (num % mul === 0) {
+        if (num === 0) {
+            break;
+        }
+        ecx++;
+        num = Math.floor(num/mul);
+    }
+    return ecx;
+}
+
+function facToArray(token) {
+    let result = [];
+    let step = token.endsWith("!!") ? 2 : 1;
+    let val=parseInt(token.replace("!", ""));
+    while (val>0){
+        result.push(val);
+        val-=step;
+    }
+    return result;
 }
